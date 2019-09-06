88b6795137fb6c532cba94f987d692824c3bac58
Kumiko Nomura <kumiko.nomura@laverne.edu>
2019-09-05 23:13:38 -0700

    restyling about me section

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -4,35 +4,29 @@ import "./About.css";
 const About = _ => {
   return (
     <>
-      <section class="page-section text-secondary mb-0" id="about">
-        <div class="container">
+      <section className="page-section text-secondary mb-0" id="about">
+        <div className="container">
           <h2
             id="about_me"
-            class="page-section-heading text-center text-uppercase"
+            className="page-section-heading text-center text-uppercase"
           >
             About Me
           </h2>
 
-          <div class="row">
-            <div id="first_paragraph" class="col-lg-4 ml-auto">
-              <p id='intro' class="lead">
+          <div className="row">
+            <div id="first_paragraph" class="container">
+              <p id='first' className="lead">
                 Hi everyone! My name is Kumiko Nomura. I am a recent graduate
                 from UC Irvine's Coding Bootcamp. Prior to the bootcamp, I
                 worked as a financial accountant. After two years of working
                 with financial statements and ledgers, I finally decided it was
                 time to take a leap of faith and pursue what I've always been
-                interested in, software development.
+                interested in, software development. 
+                I am a big believer in constant learning and growth. I enjoy challenging myself in order to obtain new skills and broaden my horizons.
               </p>
-            </div>
-            <div id="second_paragraph" class="col-lg-4 mr-auto">
-              <p class="lead">
-                I am a big believer in constant learning and growth. I enjoy
-                challenging myself in order to obtain new skills and broaden my
-                horizons.
-              </p>
+              </div>
             </div>
           </div>
-        </div>
       </section>
     </>
   );
